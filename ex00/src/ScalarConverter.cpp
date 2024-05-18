a56b6e202dcf4a49449772e2eb4d4f49f4c087ac
Shetteemah <shettimaali58@yahoo.com>
2024-05-18 13:40:51 +0200

    ex00: began implementation in ScalarConverter.cpp

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -1,2 +1,42 @@
 #include "ScalarConverter.hpp"
 
+ScalarConverter::ScalarConverter() : _arg("Default")
+{
+    std::cout << "Default contstructor initiated!" << std::endl;
+}
+
+ScalarConverter::ScalarConverter(const std::string &arg) : _arg(arg)
+{
+    std::cout << "Constructor for argument has been initiated!" << std::endl;
+    
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &copy) : _arg(copy._arg)
+{}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &src)
+{}
+
+ScalarConverter::~ScalarConverter()
+{}
+
+const std::string ScalarConverter::getArgument(const std::string &arg)
+{}
+
+char ScalarConverter::getchar() const
+{}
+
+int ScalarConverter::getint() const
+{}
+
+float ScalarConverter::getfloat() const
+{}
+
+double ScalarConverter::getdouble() const
+{}
+
+const char *ScalarConverter::IncorrectScalarException::what() const throw()
+{}
+
+std::ostream &operator<<(std::ostream &os, const ScalarConverter &scalar)
+{}
