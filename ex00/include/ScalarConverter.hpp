658402ccdb897473874156fec4ac6045eef15967
Shetteemah <shettimaali58@yahoo.com>
2024-05-18 07:55:30 +0200

    ex00: created attributes for ScalarConverter class.

--- ex00/include/ScalarConverter.hpp
+++ ex00/include/ScalarConverter.hpp
@@ -2,12 +2,31 @@
 #define SCALARCONVERTER_HPP
 
 #include <iostream>
+#include <string>
+#include <exception>
 
 class ScalarConverter
 {
     private:
+        const std::string &_param;
+        char _char;
+        int _int;
+        float _float;
+        double _double;
+        ScalarConverter();
 
     public:
-    
+        ScalarConverter(const std::string &param);
+        ScalarConverter(const ScalarConverter &copy);
+        ScalarConverter &operator=(const ScalarConverter &src);
+        ~ScalarConverter();
+
+    class IncorrectScalarException : virtual public std::exception
+    {
+        public:
+            virtual const char *what() const throw();
+    };
+
+
 };
 #endif
