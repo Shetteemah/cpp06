31b68391a049abb16c9a8b9ad0c7ae0f2fdabdb3
Shetteemah <shettimaali58@yahoo.com>
2024-05-18 13:21:41 +0200

    ex00: created getters for ScalarConverter class.

--- ex00/include/ScalarConverter.hpp
+++ ex00/include/ScalarConverter.hpp
@@ -8,7 +8,7 @@
 class ScalarConverter
 {
     private:
-        const std::string &_param;
+        const std::string &_arg;
         char _char;
         int _int;
         float _float;
@@ -16,10 +16,16 @@ class ScalarConverter
         ScalarConverter();
 
     public:
-        ScalarConverter(const std::string &param);
+        ScalarConverter(const std::string &arg);
         ScalarConverter(const ScalarConverter &copy);
         ScalarConverter &operator=(const ScalarConverter &src);
         ~ScalarConverter();
+
+        const std::string getArgument(const std::string &arg);
+        char getchar() const;
+        int getint() const;
+        float getfloat() const;
+        double getdouble() const;
 
     class IncorrectScalarException : virtual public std::exception
     {
@@ -29,4 +35,5 @@ class ScalarConverter
 
 
 };
+std::ostream &operator<<(std::ostream &os, const ScalarConverter &scalar);
 #endif
